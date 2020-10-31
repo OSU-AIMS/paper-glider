e895bc1e87c6117753ba1320267721017db82138
acbuynak <acbuynak@gmail.com>
2020-10-31 18:49:23 -0400

    Streamline Code for New Project

--- scripts/paper-glider.py
+++ scripts/paper-glider.py
@@ -81,8 +81,10 @@ def all_close(goal, actual, tolerance):
   return True
 
 
+
+
 class ThrowingArm(object):
-  """ThrowingArm"""
+  """ThrowingArm Class"""
   def __init__(self):
     super(ThrowingArm, self).__init__()
 
@@ -90,71 +92,29 @@ class ThrowingArm(object):
     moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node('node_ThrowingArm', anonymous=True)
 
-    ## Instantiate a `RobotCommander`_ object. Provides robot kinematic model and current joint states
-    robot = moveit_commander.RobotCommander()
-
-    ## Instantiate a `PlanningSceneInterface`_ object. Remote interface to get, set, update robots understanding of scene
-    scene = moveit_commander.PlanningSceneInterface()
-
-    ## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
-    ## to a planning group (group of joints). This interface can be used to plan and execute motions:
-    group_name = "bot_mh5"
-    move_group = moveit_commander.MoveGroupCommander(group_name)
-
-    ## Create a `DisplayTrajectory`_ ROS publisher which is used to display trajectories in Rviz:
-    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+    # Setup Variables needed for Moveit_Commander
+    self.box_name = ''
+    self.robot = moveit_commander.RobotCommander()
+    self.scene = moveit_commander.PlanningSceneInterface()
+    self.group_name = "bot_mh5"
+    self.move_group = moveit_commander.MoveGroupCommander(self.group_name)
+    self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
                                                    moveit_msgs.msg.DisplayTrajectory,
                                                    queue_size=20)
+    self.planning_frame = self.move_group.get_planning_frame()
+    self.eef_link = self.move_group.get_end_effector_link()
+    self.group_names = self.robot.get_group_names()
 
 
-    ## PRINT REPORT: basic_info
-    ##
-    ## Getting Basic Information
-    ## ^^^^^^^^^^^^^^^^^^^^^^^^^
-    # We can get the name of the reference frame for this robot:
-    planning_frame = move_group.get_planning_frame()
-    print "============ Planning frame: %s" % planning_frame
-
-    # We can also print the name of the end-effector link for this group:
-    eef_link = move_group.get_end_effector_link()
-    print "============ End effector link: %s" % eef_link
-
-    # We can get a list of all the groups in the robot:
-    group_names = robot.get_group_names()
-    print "============ Available Planning Groups:", robot.get_group_names()
-
-    # Sometimes for debugging it is useful to print the entire state of the
-    # robot:
-    print "============ Printing robot state"
-    print robot.get_current_state()
-    print ""
-    ## END_SUB_TUTORIAL
-
-    # Misc variables
-    self.box_name = ''
-    self.robot = robot
-    self.scene = scene
-    self.move_group = move_group
-    self.display_trajectory_publisher = display_trajectory_publisher
-    self.planning_frame = planning_frame
-    self.eef_link = eef_link
-    self.group_names = group_names
-
-
-
-
-  def go_to_js_all_zeros(self):
-    # Copy class variables to local variables to make the web tutorials more clear.
-    # In practice, you should use the class variables directly unless you have a good
-    # reason not to.
-    move_group = self.move_group
-
-    ## BEGIN_SUB_TUTORIAL plan_to_joint_state
-    ##
-    ## Planning to a Joint Goal
-    ## ^^^^^^^^^^^^^^^^^^^^^^^^
+  def goto_all_zeros(self):
+    ## Go to "ALL-Zeros" position
     ## Get Current Position & Go to "All-Zeros" Position
-    joint_goal = move_group.get_current_joint_values()
+    ## Trajectory Type: JOINT MOTION defined by joint position
+
+    # Get Current Position
+    joint_goal = self.move_group.get_current_joint_values()
+
+    # Define "All-Zeros" Position
     joint_goal[0] = 0
     joint_goal[1] = 0
     joint_goal[2] = 0
@@ -162,55 +122,69 @@ class ThrowingArm(object):
     joint_goal[4] = 0
     joint_goal[5] = 0
 
-    # Once POSE or JOINT GOALS has been set. Use GO to drive to initiate actual motion
-    move_group.go(joint_goal, wait=True)
+    # Send action to move-to defined position
+    self.move_group.go(joint_goal, wait=True)
 
     # Calling ``stop()`` ensures that there is no residual movement
-    move_group.stop()
-
-    ## END DEF
+    self.move_group.stop()
 
     # For testing:
-    current_joints = move_group.get_current_joint_values()
+    current_joints = self.move_group.get_current_joint_values()
     return all_close(joint_goal, current_joints, 0.01)
 
 
-  def go_to_pose_goal(self):
-    # Copy class variables to local variables to make the web tutorials more clear.
-    # In practice, you should use the class variables directly unless you have a good
-    # reason not to.
-    move_group = self.move_group
+  def goto_airplane_pickup(self):
+    ## Go to "ALL-Zeros" position
+    ## Get Current Position & Go to "All-Zeros" Position
+    ## Trajectory Type: JOINT MOTION defined by cartesian position
 
-    ## BEGIN_SUB_TUTORIAL plan_to_pose
-    ##
-    ## Planning to a Pose Goal
-    ## ^^^^^^^^^^^^^^^^^^^^^^^
-    ## We can plan a motion for this group to a desired pose for the
-    ## end-effector:
     pose_goal = geometry_msgs.msg.Pose()
     pose_goal.orientation.w = 0
     pose_goal.position.x = 0
     pose_goal.position.y = -0.6
     pose_goal.position.z = 0.3
 
-    move_group.set_pose_target(pose_goal)
+    self.move_group.set_pose_target(pose_goal)
 
     ## Call the planner to compute the plan and execute it.
-    plan = move_group.go(wait=True)
+    plan = self.move_group.go(wait=True)
+
     # Calling `stop()` ensures that there is no residual movement
-    move_group.stop()
+    self.move_group.stop()
     # It is always good to clear your targets after planning with poses.
     # Note: there is no equivalent function for clear_joint_value_targets()
-    move_group.clear_pose_targets()
-
-    ## END DEF
+    self.move_group.clear_pose_targets()
 
     # For testing:
-    # Note that since this section of code will not be included in the tutorials
-    # we use the class variable rather than the copied state variable
     current_pose = self.move_group.get_current_pose().pose
     return all_close(pose_goal, current_pose, 0.01)
 
+
+  def goto_joint_posn(self,joints):
+    ## Go to an Input Position
+    ## Get Current Position & Go to input position
+    ## Trajectory Type: JOINT MOTION defined by cartesian position
+
+    pose_goal = geometry_msgs.msg.Pose()
+    pose_goal.orientation.w = joints[0]
+    pose_goal.position.x = joints[1]
+    pose_goal.position.y = joints[2]
+    pose_goal.position.z = joints[3]
+
+    self.move_group.set_pose_target(pose_goal)
+
+    ## Call the planner to compute the plan and execute it.
+    plan = self.move_group.go(wait=True)
+
+    # Calling `stop()` ensures that there is no residual movement
+    self.move_group.stop()
+    # It is always good to clear your targets after planning with poses.
+    # Note: there is no equivalent function for clear_joint_value_targets()
+    self.move_group.clear_pose_targets()
+
+    # For testing:
+    current_pose = self.move_group.get_current_pose().pose
+    return all_close(pose_goal, current_pose, 0.01)
 
   def plan_cartesian_path(self, scale=1):
     # Copy class variables to local variables to make the web tutorials more clear.
@@ -274,83 +248,28 @@ class ThrowingArm(object):
     # Note: We are just planning, not asking move_group to actually move the robot yet:
     return plan, fraction
 
-    ## END_SUB_TUTORIAL
-
-
-  def display_trajectory(self, plan):
-    # Copy class variables to local variables to make the web tutorials more clear.
-    # In practice, you should use the class variables directly unless you have a good
-    # reason not to.
-    robot = self.robot
-    display_trajectory_publisher = self.display_trajectory_publisher
-
-    ## BEGIN_SUB_TUTORIAL display_trajectory
-    ##
-    ## Displaying a Trajectory
-    ## ^^^^^^^^^^^^^^^^^^^^^^^
-    ## You can ask RViz to visualize a plan (aka trajectory) for you. But the
-    ## group.plan() method does this automatically so this is not that useful
-    ## here (it just displays the same trajectory again):
-    ##
-    ## A `DisplayTrajectory`_ msg has two primary fields, trajectory_start and trajectory.
-    ## We populate the trajectory_start with our current robot state to copy over
-    ## any AttachedCollisionObjects and add our plan to the trajectory.
-    display_trajectory = moveit_msgs.msg.DisplayTrajectory()
-    display_trajectory.trajectory_start = robot.get_current_state()
-    display_trajectory.trajectory.append(plan)
-    # Publish
-    display_trajectory_publisher.publish(display_trajectory);
-
-    ## END_SUB_TUTORIAL
-
 
   def execute_plan(self, plan):
-    # Copy class variables to local variables to make the web tutorials more clear.
-    # In practice, you should use the class variables directly unless you have a good
-    # reason not to.
-    move_group = self.move_group
-
-    ## BEGIN_SUB_TUTORIAL execute_plan
-    ##
-    ## Executing a Plan
-    ## ^^^^^^^^^^^^^^^^
-    ## Use execute if you would like the robot to follow
-    ## the plan that has already been computed:
-    move_group.execute(plan, wait=True)
-
-    ## **Note:** The robot's current joint state must be within some tolerance of the
-    ## first waypoint in the `RobotTrajectory`_ or ``execute()`` will fail
-    ## END_SUB_TUTORIAL
+    ## Execute a Plan
+    ## Use execute if you would like the robot to follow a plan that has already been computed:
+    self.move_group.execute(plan, wait=True)
 
 
   def wait_for_state_update(self, box_is_known=False, box_is_attached=False, timeout=4):
-    # Copy class variables to local variables to make the web tutorials more clear.
-    # In practice, you should use the class variables directly unless you have a good
-    # reason not to.
-    box_name = self.box_name
-    scene = self.scene
+    ## wait_for_scene_update
+    ## Serves to ensure that the paper-airplane simulated object has been attached to simulated robot model
+    ## This helps with collision planning. Not as important for paper-glider project, but is best practice.
 
-    ## BEGIN_SUB_TUTORIAL wait_for_scene_update
-    ##
-    ## Ensuring Collision Updates Are Receieved
-    ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    ## If the Python node dies before publishing a collision object update message, the message
-    ## could get lost and the box will not appear. To ensure that the updates are
-    ## made, we wait until we see the changes reflected in the
-    ## ``get_attached_objects()`` and ``get_known_object_names()`` lists.
-    ## For the purpose of this tutorial, we call this function after adding,
-    ## removing, attaching or detaching an object in the planning scene. We then wait
-    ## until the updates have been made or ``timeout`` seconds have passed
     start = rospy.get_time()
     seconds = rospy.get_time()
     while (seconds - start < timeout) and not rospy.is_shutdown():
       # Test if the box is in attached objects
-      attached_objects = scene.get_attached_objects([box_name])
+      attached_objects = self.scene.get_attached_objects([self.box_name])
       is_attached = len(attached_objects.keys()) > 0
 
       # Test if the box is in the scene.
       # Note that attaching the box will remove it from known_objects
-      is_known = box_name in scene.get_known_object_names()
+      is_known = self.box_name in self.scene.get_known_object_names()
 
       # Test if we are in the expected state
       if (box_is_attached == is_attached) and (box_is_known == is_known):
@@ -362,7 +281,7 @@ class ThrowingArm(object):
 
     # If we exited the while loop without returning then we timed out
     return False
-    ## END_SUB_TUTORIAL
+
 
 
 
@@ -373,44 +292,22 @@ def main():
     print "     AIMS Lab Demo: The Paper Glider Throwing Arm!        "
     print "----------------------------------------------------------"
     print "Program Developed by ACBUYNAK. Fall 2020"
-    print "Press Ctrl-C to exit at any time"
+    print "Press Enter to advance script when prompted."
     print ""
-    print "============ Press `Enter` to initialize the MH5 Robot ..."
+    print "============ Initialize the MH5 Robot & Go-To All-Zeros Position"
     raw_input()
-    tutorial = ThrowingArm()
+    robot = ThrowingArm()
+    robot.goto_all_zeros()
 
-    print "============ First, go to a defined JOINT STATE. Such as All-Zeros [0,0,0,0,0,0]"
+    print "============ Pickup Airplane"
+    #
+    raw_input()
+    robot.goto_airplane_pickup()
+
+    print "============ Per Best Practices, return to All-Zeros Joint Position [0,0,0,0,0,0]"
     print "============ Press `Enter` to execute a movement using a 'joint state goal' ..."
     raw_input()
-    tutorial.go_to_js_all_zeros()
-
-    print "============ Next, we go to a specific end-joint location in space."
-    print "          == In this example we will go to our airplane"
-    print "          == We now set and go to a 'pose_goal' such as [0,0.2,-0.6,0.1]."
-    print "          == This is [w,x,y,z] cartesian coordinates relative to the base_link's origin."
-    print "============ Press `Enter` to execute the movement using a pose goal ..."
-    raw_input()
-    tutorial.go_to_pose_goal()
-
-    print "============ Often it is desirable to set a linear pathway to follow."
-    print "          == Now we will read our current position, then set a series"
-    print "          == of relative waypoints to navigate along. Exluding z"
-    print "          == Each waypoint consists of relative (+/-)[x,y,z] information."
-    print "          == Note: This only plans & shows the path, not actually execute."
-    print "============ Press `Enter` to plan and display a Cartesian path ..."
-    raw_input()
-    cartesian_plan, fraction = tutorial.plan_cartesian_path()
-
-    print "============ Since we have already defined a list of waypoints,"
-    print "          == it is possible to use the generated trajectory plan."
-    print "============ Press `Enter` to execute the saved path ..."
-    raw_input()
-    tutorial.execute_plan(cartesian_plan)
-
-    print "============ Finally, we return to our start point at All-Zeros [0,0,0,0,0,0]"
-    print "============ Press `Enter` to execute a movement using a 'joint state goal' ..."
-    raw_input()
-    tutorial.go_to_js_all_zeros()
+    robot.goto_all_zeros()
 
     print "============ Python based paper-glider thrower demo complete!"
   except rospy.ROSInterruptException:
@@ -420,28 +317,3 @@ def main():
 
 if __name__ == '__main__':
   main()
-
-## REFERENCE NOTES FROM TUTORIAL BY SWRI
-##
-## .. _moveit_commander:
-##    http://docs.ros.org/melodic/api/moveit_commander/html/namespacemoveit__commander.html
-##
-## .. _MoveGroupCommander:
-##    http://docs.ros.org/melodic/api/moveit_commander/html/classmoveit__commander_1_1move__group_1_1MoveGroupCommander.html
-##
-## .. _RobotCommander:
-##    http://docs.ros.org/melodic/api/moveit_commander/html/classmoveit__commander_1_1robot_1_1RobotCommander.html
-##
-## .. _PlanningSceneInterface:
-##    http://docs.ros.org/melodic/api/moveit_commander/html/classmoveit__commander_1_1planning__scene__interface_1_1PlanningSceneInterface.html
-##
-## .. _DisplayTrajectory:
-##    http://docs.ros.org/melodic/api/moveit_msgs/html/msg/DisplayTrajectory.html
-##
-## .. _RobotTrajectory:
-##    http://docs.ros.org/melodic/api/moveit_msgs/html/msg/RobotTrajectory.html
-##
-## .. _rospy:
-##    http://docs.ros.org/melodic/api/rospy/html/
-##
-## END
